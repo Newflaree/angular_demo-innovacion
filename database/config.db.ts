bda3c31e5d7b29a88b29e1b46a25a4c5ec793d4d
Camilo López <camiloj.lopez79@gmail.com>
2024-06-11 15:13:17 -0400

    [CONFIG]: Configured deploy to vercel

--- database/config.db.ts
+++ database/config.db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect( process.env.MONGO_CNN || '' );
+    await mongoose.connect( process.env.MONGO_URI || '' );
     console.log( `${ '[DATABASE.CONFIG]'.green }: Database ${ 'ONLINE'.green }` );
     
   } catch ( err ) {
